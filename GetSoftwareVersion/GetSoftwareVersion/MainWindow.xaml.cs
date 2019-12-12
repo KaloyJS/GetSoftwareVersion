53fc732b2132e810bccc93f435be48eb76d5598e
KaloyNayve <40608191+KaloyNayve@users.noreply.github.com>
2019-12-12 16:33:08 -0500

    Initial Commit
    Initial Configuration

---
+++ GetSoftwareVersion/GetSoftwareVersion/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using GetSoftwareVersion.Enum;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace GetSoftwareVersion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IntPtr windowHandle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(System.EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Adds the windows message processing hook and registers USB device add/removal notification.
+            HwndSource source = HwndSource.FromHwnd(new WindowInteropHelper(this).Handle);
+            if (source != null)
+            {
+                windowHandle = source.Handle;
+                source.AddHook(HwndHandler);
+               UsbNotification.RegisterUsbDeviceNotification(windowHandle);
+            }
+        }
+
+        public event EventHandler<DeviceChange> DeviceChanged;
+       
+        /// <summary>
+        /// Method that receives window messages.
+        /// https://stackoverflow.com/questions/16245706/check-for-device-change-add-remove-events
+        /// </summary>
+        private IntPtr HwndHandler(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            if (msg == UsbNotification.WmDevicechange)
+            {
+
+                switch ((int)wparam)
+                {
+                    case UsbNotification.DbtDeviceremovecomplete:
+                        this.DeviceChanged?.Invoke(this, DeviceChange.Remove);
+                        //OnThresholdReached();
+                        break;
+                    case UsbNotification.DbtDevicearrival:
+                        //this.DeviceChanged?.Invoke(this, DeviceChange.Arrive);
+                        DeviceInfo device = new DeviceInfo();
+                        MessageBox.Show(device.deviceType);
+                        // OnThresholdReached();
+                        break;
+                }
+            }
+
+            handled = false;
+            return IntPtr.Zero;
+        }
+      
+
+        
+
+
+
+    }
+}
